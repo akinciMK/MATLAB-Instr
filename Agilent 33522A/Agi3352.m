0045509fbff10f4fa4a1e8a90480a02a18086078
Kenan A <kenan.eee@gmail.com>
2014-11-18 20:50:14 +0200

    new addings

--- Agilent 33522A/Agi3352.m
+++ Agilent 33522A/Agi3352.m
@@ -21,45 +21,47 @@
     disp('Pulse');
     driver.DeviceSpecific.Status.Clear();
     driver.DeviceSpecific.Utility.Reset();
-    IChannelRepcap2.OutputFunction.Function = 3;
-%   IChannelRepcap2.OutputFunction.Pulse.Width = 20E-9;
-    IChannelRepcap2.OutputFunction.Pulse.Period = 0.004;
-    IChannelRepcap2.Output.Voltage.Amplitude = 1;
-    IChannelRepcap2.OutputFunction.Pulse.DutyCycle = 50;      
-    IChannelRepcap2.OutputFunction.Pulse.Transition.LeadingEdgeTime = 8.4E-9;
-    IChannelRepcap2.OutputFunction.Pulse.Transition.TrailingEdgeTime = 8.4E-9;
-    IChannelRepcap2.OutputFunction.Pulse.Width = 16E-9;
-   
+    IChannelRepcap.OutputFunction.Function = 3;
+    
+    IChannelRepcap.OutputFunction.Pulse.Period = 1;
+    IChannelRepcap.Output.Voltage.Amplitude =2.7;
+    IChannelRepcap.OutputFunction.Pulse.DutyCycle = 40;      
+    IChannelRepcap.OutputFunction.Pulse.Transition.LeadingEdgeTime = 8.4E-9;
+    IChannelRepcap.OutputFunction.Pulse.Transition.TrailingEdgeTime = 8.4E-9;
+%     IChannelRepcap.OutputFunction.Pulse.Width = 20E-6;
+    IChannelRepcap.Output.Enabled = 1;
     
     %%%--- !PULSE ----%%%%
     
     
     %%%---ARB------%%%%
-    disp('Arb Waveform Binary');
-%     driver.DeviceSpecific.Status.Clear();
-%     driver.DeviceSpecific.Utility.Reset();
-    IChannelRepcap.OutputFunction.ArbitraryWaveform.Clear();
-    
-    Data1 = [0; 1; 1];
-    Data2(1:(1e6-3)) = 0;
-    Data2 = Data2';
-    Data = [Data1;Data2];
-    
-    IChannelRepcap.OutputFunction3.ArbitraryWaveform3.LoadArbWaveformBinary('TestArb', Data);
-    driver.DeviceSpecific.System.WaitForOperationComplete(2000);
-    IChannelRepcap.Output.Voltage.Amplitude = 2;
-    IChannelRepcap.Output.Voltage.Offset.DCOffset = 0;
-    IChannelRepcap.Output.Load = 50;
-    IChannelRepcap.OutputFunction.ArbitraryWaveform.SRate.SampleRate = 250000000;
-    IChannelRepcap.OutputFunction.Function = 6;
-    IChannelRepcap.OutputFunction.ArbitraryWaveform.SelectArbWaveform('TestArb');
-    IChannelRepcap.Output.Enabled = 1;
-    IChannelRepcap2.Output.Enabled = 1;
-    
+%     disp('Arb Waveform Binary');
+% %     driver.DeviceSpecific.Status.Clear();
+% %     driver.DeviceSpecific.Utility.Reset();
+%     IChannelRepcap.OutputFunction.ArbitraryWaveform.Clear();
+%     
+%     Data1 = [0; 1; 1];
+%     Data2(1:(1e6-3)) = 0;
+%     Data2 = Data2';
+%     Data = [Data1;Data2];
+%     
+%     IChannelRepcap.OutputFunction3.ArbitraryWaveform3.LoadArbWaveformBinary('TestArb', Data);
+%     driver.DeviceSpecific.System.WaitForOperationComplete(2000);
+%     IChannelRepcap.Output.Voltage.Amplitude = 2;
+%     IChannelRepcap.Output.Voltage.Offset.DCOffset = 0;
+%     IChannelRepcap.Output.Load = 50;
+%     IChannelRepcap.OutputFunction.ArbitraryWaveform.SRate.SampleRate = 250000000;
+%     IChannelRepcap.OutputFunction.Function = 6;
+%     IChannelRepcap.OutputFunction.ArbitraryWaveform.SelectArbWaveform('TestArb');
+%     IChannelRepcap.Output.Enabled = 1;
+%     IChannelRepcap2.Output.Enabled = 1;
+%     
     %%%---! ARB------%%%%
     
-    driver.Close();
-    disp('Driver Closed');
-    clear;
-    clc;
+    
+    
+%     driver.Close();
+%     disp('Driver Closed');
+%     clear;
+%     clc;
     
